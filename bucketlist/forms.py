f21e47b8e0527f8c84b0c58acf1872e2ebd4f469
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-20 23:57:13 +0300

    Add form class definitions

--- bucketlist/forms.py
+++ bucketlist/forms.py
@@ -2,10 +2,15 @@ from flask_wtf import FlaskForm
 
 from wtforms import StringField, BooleanField, PasswordField
 
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class SignUpForm(FlaskForm):
     fullname = StringField('Fullname', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
+                                                                     EqualTo(password)])
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
