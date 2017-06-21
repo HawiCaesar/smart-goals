b1aba6b3e41530aefffb9c97b05d9b8f4bb6b894
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-21 14:44:31 +0300

    Add bucketlist test cases, Change user update test case

--- tests/test_app.py
+++ tests/test_app.py
@@ -53,25 +53,59 @@ class SmartGoalsTestCase(unittest.TestCase):
     def test_user_can_update_details(self):
         user1 = models.User("James Brown", "jamesbr@gmail.com", "ABC")
         self.assertEqual(user1.updateUser("James Brown", "jamesbrown@gmail.com"),
-                         {"name":"James Brown", "email":"jamesbrown@gmail.com"},
+                         {"name":"James Brown", "email":"jamesbrown@gmail.com",
+                          "password":"3c01bdbb26f358bab27f267924aa2c9a03fcfdb8"},
                          "User details cannot be updated")
 
     def test_user_email_is_correct_format(self):
         user2 = models.User("Lucy Deeds", "lucydeeds@gmail.com", "ABC")
         self.assertTrue('@' in user2.email, "User email is not correct")
-    
+
     # Bucket list test cases
     def test_user_can_create_bucketlist(self):
         user1_bucketlist = models.Bucketlist()
         user1_bucketlist.create_bucketlist('Career Things', 'Goals to achieve in my career')
-        self.assertEqual(user1_bucketlist.get_bucketlists, ['Career Things'],
+        self.assertEqual(user1_bucketlist.get_bucketlists(),
+                         [{"Career Things":"Goals to achieve in my career"}],
                          "Cannot create bucketlist")
 
     def test_bucketlist_returns_list(self):
         user2_bucketlist = models.Bucketlist()
         user2_bucketlist.create_bucketlist("Traveling", "Places to visit")
-        self.assertIsInstance(user2_bucketlist.get_bucketlists, ['Traveling'],
+        self.assertIsInstance(user2_bucketlist.get_bucketlists(),
+                              list,
                               "Bucketlist does not return a list")
+
+    def test_bucketlist_activity_is_of_bucketlist_class(self):
+
+        self.assertEqual(issubclass(models.Bucketlist_Activities, models.Bucketlist), True,
+                         "bucketlist activity is not an instance of bucketlist")
+    
+    def test_user_can_update_bucketlist(self):
+        user4_bucketlist = models.Bucketlist()
+        user4_bucketlist.create_bucketlist('Movie Watch', 'Must Watch these')
+        user4_bucketlist.update_bucketlist(0, 'Movie to Watch', 'Must Watch these')
+        self.assertEqual(user4_bucketlist.get_bucketlists(),
+                         [{'Movie to Watch': 'Must Watch these'}],
+                         "Cannot create bucketlist")
+    
+    def test_user_can_make_more_than_one_bucketlist(self):
+        user5_bucketlist = models.Bucketlist()
+        user5_bucketlist.create_bucketlist('Movie Watch', 'Must Watch these')
+        user5_bucketlist.create_bucketlist('Attend 5 Live events', 'Site A, has all sites')
+        self.assertEqual(user5_bucketlist.get_bucketlists(),
+                         [{'Movie Watch': 'Must Watch these'},
+                          {'Attend 5 Live events':'Site A, has all sites'}],
+                         "Cannot create more than 1 bucketlist")
+
+    def test_user_can_remove_bucketlist(self):
+        user6_bucketlist = models.Bucketlist()
+        user6_bucketlist.create_bucketlist('Must drive cars', 'Cars I must drive')
+        user6_bucketlist.create_bucketlist('Adrenaline stunts',
+                                           'Things to make the adrenaline rush')
+        self.assertEqual(user6_bucketlist.delete_bucketlist(0),
+                         [{'Adrenaline stunts': 'Things to make the adrenaline rush'}],
+                         "Cannot remove bucketlist items")
 
 
 
