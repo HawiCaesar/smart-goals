25d4e42b064d259e8041c046a2396b746042302f
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-21 10:18:46 +0300

    Change config implementation to have development and production settings

--- bucketlist/__init__.py
+++ bucketlist/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import app_config
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
@@ -8,5 +9,6 @@ Bootstrap(app)
 from bucketlist import views
 
 # Load the config file
-app.config.from_object('config')
+app.config.from_object(app_config['development'])
+
 app.config['SECRET_KEY'] = "q38FGSFDsyrefbhj54"
