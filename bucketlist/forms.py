6b3d6258e780d55988b852502fd64ce9244d1a87
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-21 14:39:32 +0300

    Add EqualTo validtor for WTForms

--- bucketlist/forms.py
+++ bucketlist/forms.py
@@ -7,9 +7,8 @@ from wtforms.validators import DataRequired, Email, EqualTo
 class SignUpForm(FlaskForm):
     fullname = StringField('Fullname', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
-                                                                     EqualTo(password)])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm_password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
