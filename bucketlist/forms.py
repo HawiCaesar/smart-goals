aec25891b0b92e15dd342333d4ca1c9a79b42684
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-22 16:43:56 +0300

    Change forms.py structure

--- bucketlist/forms.py
+++ bucketlist/forms.py
@@ -7,8 +7,9 @@ from wtforms.validators import DataRequired, Email, EqualTo
 class SignUpForm(FlaskForm):
     fullname = StringField('Fullname', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm_password')])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
+                                                                     EqualTo('password')])
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
