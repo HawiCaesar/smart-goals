6c1f707ae12fea328e2848874530e6d724f7ba3f
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-22 16:39:38 +0300

    Restructure test cases to have setup for minimal tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,47 +4,44 @@ from bucketlist import app, models
 
 class SmartGoalsTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.tester = app.test_client(self)
+
     #test if pages return 200 Ok message when called
     def test_index_page_works(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
+
+        response = self.tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 
     def test_login_page_works(self):
-        tester = app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
+        response = self.tester.get('/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 
     def test_signup_page_works(self):
-        tester = app.test_client(self)
-        response = tester.get('/sign-up', content_type='html/text')
+        response = self.tester.get('/sign-up', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 
     def test_about_page_works(self):
-        tester = app.test_client(self)
-        response = tester.get('/about', content_type='html/text')
+        response = self.tester.get('/about', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 
     def test_contact_page_works(self):
-        tester = app.test_client(self)
-        response = tester.get('/contact', content_type='html/text')
+        response = self.tester.get('/contact', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 
     def test_missing_templates_status(self):
         #Testing status code response page not found"""
-        tester = app.test_client(self)
-        response = tester.get('/xyz-page', content_type='html/text')
+        response = self.tester.get('/xyz-page', content_type='html/text')
         self.assertEqual(response.status_code, 404)
 
 
     def test_custom_message_for_page_not_found(self):
-        tester = app.test_client(self)
-        response = tester.get('/xyz-page', content_type='html/text')
+        response = self.tester.get('/xyz-page', content_type='html/text')
         self.assertTrue(b'Page Not' in response.data, msg="Custom 404 page not working")
 
 
@@ -186,7 +183,6 @@ class SmartGoalsTestCase(unittest.TestCase):
         count = len(bucketlist_activity.delete_bucketlist_activity(0))
 
         self.assertEqual(count, 1, "Cannot delete bucketlist activity")
-
 
 
 
