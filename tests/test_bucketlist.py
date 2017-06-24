05f28aeeadf0ddfc838f3000de7714d7270dc4bb
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-24 22:21:00 +0300

    Add TestCase for bucketlists, simple add update and delte test cases

---
+++ tests/test_bucketlist.py
@@ -0,0 +1,72 @@
+import unittest
+
+from bucketlist import app, models
+
+class BucketlistSmartGoalsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.tester = app.test_client(self)
+        self.user1 = models.User()
+        self.user1_bucketlist = models.Bucketlist()
+        models.all_users = {} #master user list
+        models.all_bucketlists = {} #master bucketlist list
+
+    def test_user_can_make_bucketlist(self):
+
+        self.user1.create_user("Zach Reed", "zreed@email.com", "qaz12#@")
+
+        self.user1_bucketlist.create_bucketlist(models.all_users['zreed@email.com'][1],
+                                                'Career Things',
+                                                'Goals to achieve in my career')
+
+        self.assertEqual(models.all_bucketlists['zreed@email.com'],
+                         [{"Career Things":"Goals to achieve in my career"}],
+                         "Cannot create bucketlist")
+
+    def test_user_can_make_more_than_one_bucketlist(self):
+
+        self.user1.create_user("John Ist", "jist@email.com", "qaz12#@")
+
+        self.user1_bucketlist.create_bucketlist(models.all_users['jist@email.com'][1],
+                                                'Career Things',
+                                                'Goals to achieve in my career')
+        self.user1_bucketlist.create_bucketlist(models.all_users['jist@email.com'][1],
+                                                'Travel Manenos',
+                                                'Places to travel')
+
+        count = len(models.all_bucketlists["jist@email.com"][0])
+
+        self.assertEqual(count, 2, "User cannot make more than one bucketlist")
+
+
+    def test_user_can_update_bucket_their_bucketlist(self):
+        self.user1.create_user("John Brown", "jbrown@email.com", "qaz12#@")
+
+        self.user1_bucketlist.create_bucketlist(models.all_users['jbrown@email.com'][1],
+                                                'Career Targets',
+                                                'Goals to achieve in my career')
+
+        self.user1_bucketlist.update_bucketlist(models.all_users['jbrown@email.com'][1],
+                                                'Career Targets', 'Career Targets',
+                                                'Target to aim for in my career')
+
+        self.assertEqual(models.all_bucketlists['jbrown@email.com'][0],
+                         {'Career Targets':'Target to aim for in my career'},
+                         'User cannot update their bucketlist')
+
+    def test_user_can_delete_their_bucketlist(self):
+        self.user1.create_user("Brian Hawi", "bhawi@gmail.com", "qaz12#@")
+
+        self.user1_bucketlist.create_bucketlist(models.all_users['bhawi@gmail.com'][1],
+                                                'Career Things',
+                                                'Goals to achieve in my career')
+        self.user1_bucketlist.create_bucketlist(models.all_users['bhawi@gmail.com'][1],
+                                                'Travel Manenos',
+                                                'Places to travel')
+
+        self.user1_bucketlist.delete_bucketlist(models.all_users['bhawi@gmail.com'][1],
+                                                'Career Things')
+
+        self.assertEqual(models.all_bucketlists['bhawi@gmail.com'][0],
+                         {'Travel Manenos':'Places to travel'},
+                         "User cannot delete a bucketlist")
+        
